3fc95291a1de4054bd8a69ac8415b3a7e577d896
JimmyQu <laojie420@163.com>
2019-08-06 17:38:10 +0800

    feat  MDN练习

---
+++ jq/Javascript第一步/ui.js
@@ -0,0 +1,66 @@
+window.onload = () => {
+    document.getElementById('cn').onclick = () => {
+        document.title = '笑话生成器';
+        document.getElementById('lbl-customname').textContent = '请输入自定义的名字：';
+        document.getElementById('lbl-cn').textContent = '中国';
+        document.getElementById('lbl-us').textContent = '美国';
+        document.getElementById('lbl-uk').textContent = '英国';
+        document.getElementById('customname').placeholder = '李雷';
+        document.querySelector('.randomize').textContent = '随机生成笑话';
+    };
+
+    document.getElementById('us').onclick = () => {
+
+    };
+
+    document.getElementById('uk').onclick = () => {
+        document.title = 'Silly story generator';
+        document.getElementById('lbl-customname').textContent = 'Enter custom name:';
+        document.getElementById('lbl-cn').textContent = 'CN';
+        document.getElementById('lbl-us').textContent = 'US';
+        document.getElementById('lbl-uk').textContent = 'UK';
+        document.getElementById('customname').placeholder = 'Bob';
+        document.querySelector('.randomize').textContent = 'Generate random story';
+    };
+
+    const customName = document.getElementById('customname');
+    const randomize = document.querySelector('.randomize');
+    const story = document.querySelector('.story');
+
+    
+    let insertX = ['Willy the Goblin',
+        'Big Daddy',
+        'Father Christmas']
+
+    let insertY = ['the soup kitchen',
+        'Disneyland',
+        'the White House']
+
+    let insertZ = ['spontaneously combusted',
+        'melted into a puddle on the sidewalk',
+        'turned into a slug and crawled away']
+
+    
+    
+    function randomValueFromArray(array) {
+        return array[Math.floor(Math.random() * array.length)];
+    }
+
+    let inputs = Array.from(document.getElementsByName('country'))
+    
+    randomize.addEventListener('click',()=>{
+        let [nowChecked] =inputs.filter(item=>item.checked?item.id:null)
+        console.log(nowChecked.id)
+        let storyText;
+        if(customName.value){
+            storyText = ` It was 94 fahrenheit outside, so ${randomValueFromArray(insertX)} went for a walk. When they got to ${randomValueFromArray(insertY)}:, they stared in horror for a few moments, then ${customName.value} saw the whole thing, but was not surprised — ${randomValueFromArray(insertX)} weighs 300 pounds, and it was a hot day.`
+        }else{
+            storyText = ` It was 94 fahrenheit outside, so ${randomValueFromArray(insertX)} went for a walk. When they got to ${randomValueFromArray(insertY)}:, they stared in horror for a few moments, then ${randomValueFromArray(insertZ)} saw the whole thing, but was not surprised — ${randomValueFromArray(insertX)} weighs 300 pounds, and it was a hot day.`
+        }
+      
+        story.textContent = storyText;
+        story.style.visibility = 'visible';
+        
+    })    
+
+}
