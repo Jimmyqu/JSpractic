09fee18eea2fdd642cbd97074d18660e893ad2c0
Jimmy <laojie420@163.com>
2020-08-08 17:53:30 +0800

    test

---
+++ reactPractic/reduxPro/src/NumApp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import NumInput from './component/NumInput'
+
+function App() {
+
+    return <div>
+      <NumInput width={120}></NumInput>
+    </div>
+}
+
+export default App
--- reactPractic/sega-pro/src/store/reducer.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux'
-import homeReducer from './module/home'
-import computedReducer from './module/computed'
-
-export const reducer = combineReducers({
-    homeReducer,
-    computedReducer
-})
