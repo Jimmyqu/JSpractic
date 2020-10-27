e0d684d4788cc706dfed7f48c0b4e3a8de0cb626
laojie420@163.com <laojie420@163.com>
2020-10-27 18:11:37 +0800

    canvas

---
+++ vuePractice/vite-app/vue3-ui/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <button @click="handleAdd">++++</button>
+  </div>
+</template>
+
+<script>
+   import { ref } from 'vue'
+export default {
+  setup() {
+    const message = ref('Hello 3.0!');
+
+    const handleAdd = ()=>{
+      message.value +='1' 
+    }
+    return {
+        message,
+        handleAdd
+    }
+  }
+};
+</script>
